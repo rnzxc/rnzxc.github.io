ad01fda89b78d0f2e0ed259ff43b176452bc2b9e
rnzxc <lost+found@rnzxc.intranet>
2024-02-18 13:07:09 +0100

    Shitty windows reserved keywords.  We've had to modify this shit once again.

--- serve.bat
+++ serve.bat
@@ -1,14 +1,16 @@
 @echo off
 
+setlocal
+
 rem Simple Local HTTP Bootstrap via PHP
 rem because, we can't deal with bullshit.
 
-set PROT=http
-set HOST=127.0.0.1
-set PORT=5000
-set PATH=/index.htm
-set LISTEN=0.0.0.0
+set _PROT=http
+set _HOST=127.0.0.1
+set _PORT=5000
+set _PATH=/index.htm
+set _LISTEN=0.0.0.0
 
-start %PROT%://%HOST%:%PORT%%PATH%
-php -S %LISTEN%:%PORT% -t "%CD%"
+start %_PROT%://%_HOST%:%_PORT%%_PATH%
+php -S %_LISTEN%:%_PORT% -t "%CD%"
 exit /b %ERRORLEVEL%
